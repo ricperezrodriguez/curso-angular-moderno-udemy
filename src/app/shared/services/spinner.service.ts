be5363bc1dbc85b6c52f2f5f5109750b296ea16b
bezael <bezael@gmail.com>
2024-11-25 18:56:53 +0100

    remove comments

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -2,16 +2,13 @@ import { Injectable, signal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class SpinnerService {
-  // private isLoadingSubject = new BehaviorSubject<boolean>(false);
   readonly isLoading = signal<boolean>(false);
 
   show(): void {
     this.isLoading.set(true);
-    // this.isLoadingSubject.next(true);
   }
 
   hide(): void {
     this.isLoading.set(false);
-    // this.isLoadingSubject.next(false);
   }
 }
